b3d1ff724ad5267e451dd1f608fad96f2e897f5a
Brady Wied <brady@bswtechconsulting.com>
2018-02-12 22:33:17 -0700

    Script now runs properly

--- lib/bsw_tech/jenkins_gem/bundler_copy.rb
+++ lib/bsw_tech/jenkins_gem/bundler_copy.rb
@@ -3,23 +3,23 @@ require 'yaml'
 require 'fileutils'
 require 'bsw_tech/jenkins_gem/update_json_parser'
 
-# TODO: When gem hpi files are added, and upload to Gemfury
-# TODO: Provide a separate one off command to download an hpi and upload a built gem for that to fury. can use previous code that interprets manifest
-# TODO: Use bins for current shell script
-dir = ENV['PLUGIN_DEST_DIR']
-fail 'Specify PLUGIN_DEST_DIR env variable' unless dir && !dir.empty?
-FileUtils.rm_rf dir
-FileUtils.mkdir_p dir
+def copy_gems
+  # TODO: Provide a separate one off command to download an hpi and upload a built gem for that to fury. can use previous code that interprets manifest
+  dir = ENV['PLUGIN_DEST_DIR']
+  fail 'Specify PLUGIN_DEST_DIR env variable' unless dir && !dir.empty?
+  FileUtils.rm_rf dir
+  FileUtils.mkdir_p dir
 
-Bundler.load.specs.select do |s|
-  s.name.start_with?('jenkins') && !s.name.include?(BswTech::JenkinsGem::UpdateJsonParser::JENKINS_CORE_PACKAGE)
-end.each do |s|
-  # It's odd this is called to_yaml, but it does in fact load the gemspec that we can retrieve metadata from
-  gem_spec = YAML.load s.to_yaml
-  jenkins_name = gem_spec.metadata[BswTech::JenkinsGem::UpdateJsonParser::METADATA_JENKINS_NAME]
-  source_path = s.full_gem_path
-  dest_path = File.join(dir, "#{jenkins_name}.hpi")
-  FileUtils.cp_r(source_path, dest_path)
-  # Jenkins insists on this timestamp file
-  FileUtils.touch File.join(dest_path, '.timestamp2')
+  Bundler.load.specs.select do |s|
+    s.name.start_with?('jenkins') && !s.name.include?(BswTech::JenkinsGem::UpdateJsonParser::JENKINS_CORE_PACKAGE)
+  end.each do |s|
+    # It's odd this is called to_yaml, but it does in fact load the gemspec that we can retrieve metadata from
+    gem_spec = YAML.load s.to_yaml
+    jenkins_name = gem_spec.metadata[BswTech::JenkinsGem::UpdateJsonParser::METADATA_JENKINS_NAME]
+    source_path = s.full_gem_path
+    dest_path = File.join(dir, "#{jenkins_name}.hpi")
+    FileUtils.cp_r(source_path, dest_path)
+    # Jenkins insists on this timestamp file
+    FileUtils.touch File.join(dest_path, '.timestamp2')
+  end
 end
